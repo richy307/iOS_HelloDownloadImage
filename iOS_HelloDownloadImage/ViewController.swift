27adba496a4712ed75c7a7dfaf8f2fb371172c23
Richy <richy@webcomm.com.tw>
2023-08-02 15:00:40 +0800

    session?.dataTask

--- iOS_HelloDownloadImage/ViewController.swift
+++ iOS_HelloDownloadImage/ViewController.swift
@@ -22,6 +22,25 @@ class ViewController: UIViewController {
         // String -> URL -> Data -> UIImage
         let imageAddress:String = "https://inapp.com/wp-content/uploads/elementor/thumbs/swift-01-q05qmwpi08qczw6n86y04p8o0662t41v843uww1xoy.png"
         if let imageURL = URL(string: imageAddress){
+            /*
+            // dataTask: 文字資料, XML, JSON, string // return data
+            let task = session?.dataTask(with: imageURL, completionHandler: {
+                // urlResponse:200, 404
+                (data, urlResponse, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                    return
+                }
+                
+                if let loadedData = data {
+                    let loadedImage = UIImage(data: loadedData)
+                    DispatchQueue.main.async {
+                        self.myImageView.image = loadedImage
+                    }
+                }
+            })
+            task?.resume() // 開始下載圖片
+            */
             
             // global queue 共時佇列
             DispatchQueue.global().async {
