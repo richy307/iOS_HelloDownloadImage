c776d66d3e4cc72c47c6b4829e46370e1d3fdb5b
Richy <richy@webcomm.com.tw>
2023-08-02 11:14:03 +0800

    download url image

--- iOS_HelloDownloadImage/ViewController.swift
+++ iOS_HelloDownloadImage/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // String -> URL -> Data -> UIImage
+        let imageAddress:String = "https://cdn3.macworld.co.uk/cmsdata/features/3523633/swift_1200home_thumb800.jpg"
+        if let imageURL = URL(string: imageAddress){
+            do{
+                let imageData = try Data(contentsOf: imageURL)
+                myImageView.image = UIImage(data: imageData)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
     }
 
 
